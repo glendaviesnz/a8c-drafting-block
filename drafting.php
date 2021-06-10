9730c93667864e3b3a0abdf5b0f6ff5dba21edab
Glen Davies <glen.davies@a8c.com>
2021-06-11 09:36:11 +1200

    Tidy up

--- drafting.php
+++ drafting.php
@@ -5,18 +5,12 @@
  * Requires at least: 5.7
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            @glendaviesnz
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       draft-block
  *
  * @package           a8c-draft-block
- */
-
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
  */
 function a8c_draft_block_init() {
 	register_block_type_from_metadata( __DIR__ );
