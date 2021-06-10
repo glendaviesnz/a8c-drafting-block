c034e275a71fcf2fccc3cdee57ab2ef9d92105f5
Glen Davies <glen.davies@a8c.com>
2021-06-11 09:49:01 +1200

    Change icon

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
  * WordPress dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
+import { pencil as icon } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -11,6 +12,7 @@ import edit from './edit';
 import save from './save';
 
 registerBlockType('a8c/draft', {
+	icon,
 	edit,
 	save,
 });
